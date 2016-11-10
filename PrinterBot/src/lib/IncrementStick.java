3454f0720eaa244e32fb6d3999f7ef7e0ed08768
coalman321 <coalman321@users.noreply.github.com>
2016-11-09 20:13:54 -0500

    Update IncrementStick.java

--- PrinterBot/src/lib/IncrementStick.java
+++ PrinterBot/src/lib/IncrementStick.java
@@ -1,7 +1,19 @@
 package lib;
 
 public class IncrementStick {
-	public IncrementStick(){
-		
+	private int count;
+	private int val;
+	public IncrementStick(int countBy, int begin){
+		count = begin;
+		val=countBy;
 	}
+	public int incVal(boolean inc){
+		count+=val;
+		return count;
+	}
+	public int decVal(boolean dec){
+		count-=val;
+		return count;
+	}
+		
 }
